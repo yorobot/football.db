9cb917baabae2b0ca026d28703f58f4da86c5e6b
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-07-05 20:13:16 +0200

    up mirror

--- mirror.rb
+++ mirror.rb
@@ -1,5 +1,5 @@
 ####
-#  mirror england datasets - export to csv
+#  mirror datasets - export to csv
 
 require_relative 'boot'
 
@@ -20,7 +20,9 @@ class CsvMatchWriter
           f.write headers.join(',')   ## e.g. Date,Team 1,FT,HT,Team 2
           f.write "\n"
           recs.each do |rec|
-              f.write rec.join(',')
+              ## get values from record (use empty string if not found)
+              values = headers.map {|header| rec[header] || '' }
+              f.write values.join(',')
               f.write "\n"
           end
         end
@@ -45,41 +47,75 @@ SportDb::Model::Event.order( :id ).each do |event|
   ## note: in eng - skip playoffs for now e.g. eng.playoffs.2, eng.playoffs.3 etc.
   ## note: in de  - skip DFB Pokal (de.cup) and Relegation (de.rel) for now
   next if event.league.key.start_with?( 'eng.playoffs' )
+
   next if event.league.key.start_with?( 'de.rel' )
   next if event.league.key.start_with?( 'de.cup' )
 
+  next if event.league.key.start_with?( 'at.rel' )
+  ## next if event.league.key.start_with?( 'at.cup' )
 
-  puts "write #{event.league.key} - #{event.season.key}"
+
+  ## check for stages
+  stage_count  = event.stages.count
+  match_count  = event.matches.count
+
+  puts "write #{event.league.key} - #{event.season.key}  (#{match_count} matches, #{stage_count} stages)"
+
 
   recs = []
-  event.rounds.each do |round|
-     matchday = if round.name =~ /(?:Round|Matchday|Spieltag) ([0-9]+)/
+  event.matches.each do |match|
+
+     ## try to find / use matchday for "simple" rounds
+     ##   otherwise use round name as is
+     matchday = if match.round.name =~ /(?:Round|Matchday|Spieltag|Runde) ([0-9]+)/ ||
+                   match.round.name =~ /([0-9]+)\. (?:Runde|Spieltag)/
                   $1
                 else
-                 puts "!! ERROR - invalid matchday format >#{round.name}<:"
-                 pp round
-                 exit 1
-
-                 ## todo/check: just report errror (and continue) and set to '?' - why? why not?
+                  nil
                 end
-     round.matches.each do |match|
-         ## todo/fix: add support for cancelled/canceled, awarded (awd.), etc.
-         recs << [matchday,
-                  match.date.strftime('%Y-%m-%d'),
-                  match.team1.name,
-                  match.score1 && match.score2 ? "#{match.score1}-#{match.score2}" : '',
-                  match.team2.name]
-     end
-  end
-  puts "     #{recs.size} records"
 
-  headers = [
-     'Matchday',
-     'Date',
-     'Team 1',
-     'FT',
-     'Team 2'
-  ]
+    round = if matchday
+              matchday
+            else
+              ## note: change comma to pipe if present e.g.
+              ##  Finale, Hinspiel  =>  Finale | Hinspiel etc.
+              ##   note auto-add a space now after comma too!!!
+              match.round.name.gsub( ',', ' |' )
+            end
+
+    ## todo/fix: add support for cancelled/canceled, awarded (awd.), etc.
+    ## todo/fix: add support for ET and PT too!!!
+
+    rec = {'Round'  => round,
+           'Date'   => match.date.strftime('%Y-%m-%d'),
+           'Team 1' => match.team1.name,
+           'FT'     => match.score1 && match.score2 ? "#{match.score1}-#{match.score2}" : '',
+           'Team 2' => match.team2.name }
+
+   ## todo/fix: later shorten known stage names
+   if stage_count > 0
+    rec[ 'Stage' ] = match.stage.name
+    ## todo/fix: report error if no stage assigned to match!!!
+   end
+
+    recs << rec
+  end # each match
+
+
+  headers = if stage_count > 0
+             ['Stage',
+              'Round',
+              'Date',
+              'Team 1',
+              'FT',
+              'Team 2']
+            else
+             ['Round',    ## use matchday if only numbers - why? why not?
+              'Date',
+              'Team 1',
+              'FT',
+              'Team 2']
+            end
 
   basename = event.league.key
   season = Season.new( event.season.key )
@@ -88,11 +124,12 @@ SportDb::Model::Event.order( :id ).each do |event|
   ## path = "./o/#{season_path}/#{basename}.csv"
   path = "../../footballcsv/#{reponame}/#{season_path}/#{basename}.csv"
   puts "  path=>#{path}<"
+  ## puts "     #{recs.size} records"
 
   ## sort by date
-  recs = recs.sort { |l,r| l[1] <=> r[1] }
+  recs = recs.sort { |l,r| l['Date'] <=> r['Date'] }
   ## reformat date / beautify e.g. Sat Aug 7 1993
-  recs.each { |rec| rec[1] = Date.strptime( rec[1], '%Y-%m-%d' ).strftime( '%a %b %-d %Y' ) }
+  recs.each { |rec| rec['Date'] = Date.strptime( rec['Date'], '%Y-%m-%d' ).strftime( '%a %b %-d %Y' ) }
 
   Mirror::CsvMatchWriter.write( path, recs, headers: headers )
 end
@@ -100,7 +137,8 @@ end # method mirror
 
 
 ## mirror( dbname: 'en', reponame: 'england' )
-mirror( dbname: 'de', reponame: 'deutschland' )
+## mirror( dbname: 'de', reponame: 'deutschland' )
+mirror( dbname: 'at', reponame: 'austria' )
 
 
 puts "bye"
